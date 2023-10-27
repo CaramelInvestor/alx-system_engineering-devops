49090d660d8eff5f534c8e9444cfb714b9bb11a5
UgonmaNkwocha <caramelinvestor@gmail.com>
2023-10-27 22:39:32 +0100

    Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Install an especific version of flask (2.1.0)
-package { 'flask':
+#!/usr/bin/pup
+# Install flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package {'Werkzeug':
+  ensure   => '2.1.1.',
   provider => 'pip3'
 }
